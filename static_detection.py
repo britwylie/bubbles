8a7e857e8859c3a6b04947b34930f5c38e6e5ff4
Brit Wylie <wybrit98@gmail.com>
2019-07-29 11:58:58 -0700

    test for static image

---
+++ static_detection.py
@@ -0,0 +1,16 @@
+#! usr/bin/env python
+
+'''
+ tracking.py: 
+ Code run on Raspberry Pi to track bubbles
+
+ '''
+
+__author__ = "Brit Wylie"
+__version__ = "0.0.1"
+__maintainer__ = "Brit Wylie"
+__email__ = "bwylie@caltech.edu"
+__status__ = "Started"
+
+import cv2
+import
