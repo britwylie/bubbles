ae7990ae2b7a1f1cde532dea39196f5e4e4bdf98
Brit Wylie <wybrit98@gmail.com>
2019-07-29 12:02:10 -0700

    Added argument parsing

--- static_detection.py
+++ static_detection.py
@@ -12,5 +12,13 @@ __maintainer__ = "Brit Wylie"
 __email__ = "bwylie@caltech.edu"
 __status__ = "Started"
 
+# packages
 import cv2
-import
+import numpy as np
+from matplotlib import pyplot as plt
+import argparse
+
+# cmd arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", help = "path to the image")
+args = vars(ap.parse_args())
