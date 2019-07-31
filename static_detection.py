e29410d830f806853fc2548b9fdc3f6be9a6ae8d
Brit Wylie <wybrit98@gmail.com>
2019-07-31 15:48:11 -0700

    Canny Edge detection added to static_detection and video_detection. Tracking stil fails

--- static_detection.py
+++ static_detection.py
@@ -18,6 +18,20 @@ import numpy as np
 from matplotlib import pyplot as plt
 import argparse
 
+
+# function definitions
+
+def auto_canny(image, sigma=0.33):
+	# compute the median of the single channel pixel intensities
+	v = np.median(image)
+	# apply automatic Canny edge detection using the computed median
+	lower = int(max(0, (1.0 - sigma) * v))
+	upper = int(min(255, (1.0 + sigma) * v))
+	edged = cv2.Canny(image, lower, upper)
+ 
+	# return the edged image
+	return edged
+
 # cmd arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", help = "path to the image")
@@ -25,18 +39,23 @@ args = vars(ap.parse_args())
 
 img = cv2.imread(args["image"])
 cv2.namedWindow('image')
-output = img.copy()
+hough_output = img.copy()
+contour_output = img.copy()
 # grayscale version of jpg
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# blurred version of jpg
-blur = cv2.blur(gray, (3,3))
+blur = cv2.GaussianBlur(gray, (5,5), 0)
+_, thresh = cv2.threshold(blur, 5, 255, cv2.THRESH_BINARY_INV)
+dilated = cv2.dilate(thresh, None, iterations = 3)
+
+
+# canny edge
+edges = auto_canny(blur)
 
 # detect circles in the image
-circles = cv2.HoughCircles(blur,cv2.HOUGH_GRADIENT, 1,15, param1 = 130, param2 = 20, minRadius = 2, maxRadius =50 )
+circles = cv2.HoughCircles(blur,cv2.HOUGH_GRADIENT, 1, 15, param1 = 130, param2 = 20, minRadius = 2, maxRadius =50 )
  
-# set threshold 
-thresh = cv2.threshold(gray, 5, 255, cv2.THRESH_BINARY_INV)[1]
-gray, contours = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+# detect contours
+contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 
 
@@ -48,13 +67,29 @@ if circles is not None:
 		# draw the circle in the output image, then draw a rectabgle
 		# corresponding to the center of the circle
 		#cv2.circle(output, (x, y), r, (0, 255, 0), 1)
-		cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), 1)
+		cv2.rectangle(hough_output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), 1)
  
 	# show the output image
-	cv2.imshow("output", np.hstack([img, output]))
+	cv2.imshow("output", np.hstack([img, hough_output]))
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
+if contours is not None:
 
+	# min enclosing circle for contours
+	contour_list = []
+	for contour in contours:
+		# (x, y), radius = cv2.minEnclosingCircle(contour)
+		if cv2.contourArea(contour) < 10:
+			continue
+		
+		contour_list.append(contour)
+		
+		# cv2.circle(contour_output, (int(x), int(y)), int(radius), (0, 0, 255), 1)
+
+	# add contours to copy of img
+	cv2.drawContours(contour_output, contour_list, -1, (0,255, 0), 1)
+	cv2.imshow("contours", np.hstack([img, contour_output]))
+	cv2.waitKey(0)
 cv2.destroyAllWindows()
 '''
 while(1):
--- video_detection.py
+++ video_detection.py
@@ -3,6 +3,8 @@
 '''
  video_detection.py: 
  Code run on test video to track bubbles
+
+ Not as useful for this application, will be cannibalized and mixed with static_detection
 '''
 
 import cv2
@@ -17,6 +19,17 @@ def close(c1, list):
 			return True
 	return False		
 
+# computes boundaries for canny edge detection
+def auto_canny(image, sigma=0.33):
+	# compute the median of the single channel pixel intensities
+	v = np.median(image)
+	# apply automatic Canny edge detection using the computed median
+	lower = int(max(0, (1.0 - sigma) * v))
+	upper = int(min(255, (1.0 + sigma) * v))
+	edged = cv2.Canny(image, lower, upper)
+ 
+	# return the edged image
+	return edged
 #def inside(c1, list):
 
 
@@ -30,16 +43,27 @@ ret, frame2 = cap.read()
 
 
 while cap.isOpened():
+
+	# edge detection method 1 	
 	diff = cv2.absdiff(frame1, frame2)
 	gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
 	blur = cv2.GaussianBlur(gray, (5,5), 0)
 	_, thresh = cv2.threshold(blur, 10, 255, cv2.THRESH_BINARY)
 	dilated = cv2.dilate(thresh, None, iterations = 3)
-	contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	
+	# canny edge detection
+	edges = auto_canny(blur)
 
+	# contour detection
+	contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+	# filter through contours to get circles
 	contour_list = []
 	lengths = []
 	for contour in contours:
+
+		# approxPolyDP method
+		'''
 		approx = cv2.approxPolyDP(contour, 0.01*cv2.arcLength(contour, True), True)
 		area = cv2.contourArea(contour)
 		l = len(approx)
@@ -48,7 +72,10 @@ while cap.isOpened():
 			(x, y), radius = cv2.minEnclosingCircle(contour)
 			cv2.putText(frame1, "{}".format('Bubble'), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1)
 			cv2.circle(frame1, (int(x), int(y)), int(radius), (0, 0, 255), 1)
-		'''min enclosing circle method
+		'''
+
+		# min enclosing circle method
+
 		(x, y), radius = cv2.minEnclosingCircle(contour)
 		if cv2.contourArea(contour) < 200 or cv2.contourArea(contour) > 5000:
 			continue
@@ -57,7 +84,7 @@ while cap.isOpened():
 		contour_list.append(contour)
 		
 		cv2.circle(frame1, (int(x), int(y)), int(radius), (0, 0, 255), 1)
-		'''
+		
 	
 
 	# draw all the contours over bubbles
