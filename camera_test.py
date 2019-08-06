cdc3bd5dad93d9d811bbaa8934f2f352147a4565
Brit W <wybrit98@gmail.com>
2019-08-06 16:41:40 -0700

    header

---
+++ camera_test.py
@@ -0,0 +1,13 @@
+
+'''camera_test.py: first test of camera on Raspberry Pi to determine focal length
+
+__author__  =   "Brit W"
+__project__ =   "bubbles"
+__status__  =   "Practice only"
+
+
+'''
+
+import numpy as np
+import cv2
+
