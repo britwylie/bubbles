62db203623bc10e10b1806274e2f9a3dd8e9079b
Brit W <wybrit98@gmail.com>
2019-08-06 18:05:12 -0700

    Test files

--- camera_test.py
+++ camera_test.py
@@ -22,6 +22,12 @@ camera.start_preview()
 time.sleep(5)
 camera.capture('/home/pi/Documents/bubbles/imag.jpg')
 camera.stop_preview()
+
+camera.resolution = (640, 480)
+camera.start_recording('test_video.h264')
+camera.wait_recording(5)
+camera.stop_recording()
+
 print('Done')
 
 
